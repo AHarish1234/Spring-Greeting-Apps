e406763c9171416abe0a9b054678f3a7f6a7e7f8
AHarish1234 <aharish73944@gmail.com>
2025-05-23 12:43:46 +0530

    added greeting api

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Greeting;
+import com.example.demo.service.GreetingService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+    
+	
+	
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping
+    public Greeting getGreeting() {
+        return greetingService.getGreetingMessage("get");
+    }
+
+    @PutMapping
+    public Greeting putGreeting() {
+        return greetingService.getGreetingMessage("put");
+    }
+
+    @PostMapping
+    public Greeting postGreeting() {
+        return greetingService.getGreetingMessage("post");
+    }
+
+    @DeleteMapping
+    public Greeting deleteGreeting() {
+        return greetingService.getGreetingMessage("delete");
+    }
+
+    
+    
+
+
+}
