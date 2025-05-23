f9e06754a45271ae1e270778955450a9cc50d95f
AHarish1234 <aharish73944@gmail.com>
2025-05-23 13:05:32 +0530

    added updating the data

--- src/main/java/com/example/demo/service/GreetingService.java
+++ src/main/java/com/example/demo/service/GreetingService.java
@@ -58,6 +58,13 @@ public class GreetingService {
     public List<Greeting> findAll() {
         return greetingRepository.findAll();
     }
+    public Greeting update(long id, Greeting greeting) {
+        if (!greetingRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Greeting not found");
+        }
+        greeting.setId(id);
+        return greetingRepository.save(greeting);
+    }
 
 
 
